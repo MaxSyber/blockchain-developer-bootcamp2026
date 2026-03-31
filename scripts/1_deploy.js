a7f099770a72c239ed995c73b47cd0d16232187d
Brett Seibert <brs5241psu@gmail.com>
2026-03-31 19:44:01 -0400

    2 Smoke Test

---
+++ scripts/1_deploy.js
@@ -0,0 +1,17 @@
+async function main() {
+  console.log(`Preparing deployment...\n`)
+  //fetch contract to deploy
+  const Token = await ethers.getContractFactory("Token")
+
+  //deploy token
+  const token= await Token.deploy()
+  await token.deployed()
+  console.log(`Token Deployed to: ${token.address}`)
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
