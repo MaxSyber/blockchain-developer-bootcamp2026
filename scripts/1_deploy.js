262a7867b62589c4c8694ea6254887558d27e4e6
Brett Seibert <brs5241psu@gmail.com>
2026-04-05 15:38:13 -0400

    Final Upload for New Crypto

--- scripts/1_deploy.js
+++ scripts/1_deploy.js
@@ -4,7 +4,7 @@ async function main() {
   const Token = await ethers.getContractFactory("Token")
 
   //deploy token
-  const token= await Token.deploy()
+  const token= await Token.deploy('Dah', 'DAH', '1000000')
   await token.deployed()
   console.log(`Token Deployed to: ${token.address}`)
 }
